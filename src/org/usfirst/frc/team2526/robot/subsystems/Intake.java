7a7df19e248a7083e5d0b1921dbed38b233dc8e6
Muhammad Ahsan <mtahsan00@gmail.com>
2018-02-08 18:40:36 -0600

    Add code for intake

---
+++ src/org/usfirst/frc/team2526/robot/subsystems/Intake.java
@@ -0,0 +1,26 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team2526.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * An example subsystem.  You can replace me with your own Subsystem.
+ */
+public class Intake extends Subsystem {
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
+	WPI_TalonSRX talon1;
+	WPI_TalonSRX talon2;
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+	}
+}
